a3a02426bc9802adc0588bd2273d60e6938399a7
Self_Improve <improve@192.168.2.23>
2017-02-24 13:55:18 +0800

    Initial Commit

---
+++ 渐变色圆环/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  渐变色圆环
+//
+//  Created by Self_Improve on 17/2/24.
+//  Copyright © 2017年 ZWiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 渐变色圆环/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  渐变色圆环
+//
+//  Created by Self_Improve on 17/2/24.
+//  Copyright © 2017年 ZWiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
